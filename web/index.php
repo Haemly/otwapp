2474206990d84357f976bd45ce1212353e99cac4
Cory Musgrave <ccoryllose@gmail.com>
2014-10-18 16:10:34 -0300

    updated css and index
    Added map and  even log interface

--- web/index.php
+++ web/index.php
@@ -31,25 +31,35 @@ $(document).ready(function(){
 			} else{
 				//TODO connect to php file
 				$("#popout-background").fadeOut("slow");
-				$("#content-container").css("display" , "block");
 			}
+		});
+		
+		$("#button1").click(function(){
+			$("#event-content").append("<div class=\"event\"></div>");
+			$(".event").text("Wait requested");
+		});
+		
+		$("#button2").click(function(){
+			$("#event-content").append("<div class=\"event\"></div>");
+			$(".event").text("Continue requested");
 		});
 	});
 </script>
 <script type="text/javascript"
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB8nP8zW5doATgHCBiJCaGe4r5cNeS1V1g">
-    </script>
-    <script type="text/javascript">
-      function initialize() {
-        var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB8nP8zW5doATgHCBiJCaGe4r5cNeS1V1g&sensor=false">
+</script>
+<script type="text/javascript">
+  function initialize() {
+	var mapOptions = {
+	  center: { lat: -34.397, lng: 150.644},
+	  zoom: 6
+	};
+	var map = new google.maps.Map(document.getElementById('map-canvas'),
+		mapOptions);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+
 </head>
 <body>
 	<div id="popout-background">
@@ -62,6 +72,7 @@ $(document).ready(function(){
 	</div>
 	<div id="content-container">
 		<div id="control-content">
+		controls
 			<div id="button-cluster">
 				<div id="button1" class="control-buttons"><div class="inner-button-text">add this</div></div>
 				<div id="button2" class="control-buttons"><div class="inner-button-text">add this</div></div>
