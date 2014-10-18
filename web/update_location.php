3e847b386d91f2d3a36f6ac1d9152f977202a9f4
JPTC <j-p-t-c@hotmail.com>
2014-10-18 14:06:32 -0300

    learned hot to save all before sync
    no more backtracking

--- web/update_location.php
+++ web/update_location.php
@@ -3,7 +3,32 @@
 
 
 
+require_once('DBinfo.php');
 
+//incoming code from iphone, deserialize and test for duplicate in database
+$json = file_get_contents('php://input');
+$obj = json_decode($json);
+
+//variables
+$userCode = "";
+$first_name = ""; 
+$last_name = ""; 
+$phone_number = ""; 
+$timestamp = "";
+$error_msg = "";
+ 
+ //get variables from iphone data
+ foreach ($obj as $Field) {
+	$usercode = $Field["usercode"];
+	$first_name = $Field["first_name"];
+	$last_name = $Field["last_name"];
+	$phone_number = $Field["phone_number"];
+	$timestamp = $Field["timestamp"];
+ }
+
+//test for missing data 
+if (!empty($usercode) && !empty($first_name) && !empty($last_name) 
+	&& !empty($phone_number) && !empty($timestamp) {
 
 
 
