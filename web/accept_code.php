e86f8776a39d9455f496fd6b42806137932182cb
JPTC <j-p-t-c@hotmail.com>
2014-10-18 13:25:50 -0300

    just because
    did a few things

--- web/accept_code.php
+++ web/accept_code.php
@@ -18,21 +18,23 @@ $error_msg = "";
  
  //get variables from iphone data
  foreach ($obj as $Field) {
-	$userCode = $Field["usercode"];
+	$usercode = $Field["usercode"];
 	$first_name = $Field["first_name"];
 	$last_name = $Field["last_name"];
 	$phone_number = $Field["phone_number"];
 	$timestamp = $Field["timestamp"];
  }
 
-//test
+//test for missing data 
 if (!empty($usercode) && !empty($first_name) && !empty($last_name) 
 	&& !empty($phone_number) && !empty($timestamp) {
 	
-	//connect to database
+	//connect to database or error
 	$conn = mysql_connect("$db_name","$username","$password");
 	msyql_select_db("$db_name", $conn ) or die("E01"); 
 	
+	//query database for duplicate codes
+	$sql_query_existing = sprintf("INSERT INTO usercodes (usercode, first_name, last_name, phone_number, timestamp) VALUES ('%s','%s','%s','%s','%s',)", mysql_real_escape_string());
 	
 	
 	
